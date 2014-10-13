97e3d279dc610c79cda6612a37cdb877e6827eb3
wzj <wangzhaojiang2013@gmail.com>
2014-10-13 18:46:22 +0800

    add some new elements

--- monitor/monitor/state/views.py
+++ monitor/monitor/state/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 import os
+import re
 
 # Create your views here.
 
@@ -8,14 +9,40 @@ def hello(request):
     return render_to_response('hello.html')
 
 def info(request):
-    etc_issue = open('/etc/issue','r')
-    issue = etc_issue.readlines()
-    issue = ' '.join(issue)
+    Basic = Basicinfo()
     content = []
-    content.append(issue)
-    etc_issue.close()
-    uname = os.popen('uname -a').readlines()
-    uname = ' '.join(uname)
-    content.append(uname)
-    return render_to_response('info.html', {'content': content})
+    content.append(Basic._issue())
+    content.append(Basic._uname())
+    print Basic._ifconfig()
+    return render_to_response('info.html', {'content': content, 'w': Basic._w(), 'netstat': Basic._netstat(),})
     
+class Basicinfo():
+    """the basic infomation of the computer"""
+    def __init__(self):
+        pass
+    
+    def _issue(self):
+        issue = open('/etc/issue', 'r')
+        content = issue.readlines()
+        return ''.join(content) 
+
+    def _uname(self):
+        uname = os.popen('uname -a').readlines()
+        return ''.join(uname)
+
+    def _w(self):
+        w = os.popen('w').readlines()
+        return w
+
+    def _netstat(self):
+        netstat = os.popen('netstat -tunlp').readlines()
+        return netstat
+
+    def _ifconfig(self):
+        ifconfig = os.popen('ifconfig').readlines()
+        interface = []
+        for tip in ifconfig:
+            al = re.findall('^[^ ]\w+', tip)
+            if al :
+                interface.append(al)
+        
