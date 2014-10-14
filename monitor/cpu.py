8ed869875d083ee53fa782193e29d534e3ece48a
wzj <wangzhaojiang2013@gmail.com>
2014-10-14 21:22:14 +0800

    cpu

---
+++ monitor/cpu.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#  ------------------------------------
+#  Create date : 2014-10-14 21:00
+#  Author : Wangzhaojiang
+#  Email : wangzhaojiang2013@gmail.com
+#  ------------------------------------
+
+import re
+
+
+stat = open('/proc/stat','r').readlines()
+for tip in content:
+    tip = tip.split()
+
