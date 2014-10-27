bca43b042ea483f65f2f5f583dd833c8c0c5a400
wzj <wangzhaojiang2013@gmail.com>
2014-10-27 16:46:34 +0800

    SQL

--- monitor/monitor/settings.py
+++ monitor/monitor/settings.py
@@ -59,8 +59,13 @@ WSGI_APPLICATION = 'monitor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'monitor',
+        'USER': 'root',
+        'PASSWORD': 'notamaiba'
+        'HOST': '127.0.0.1'
+        'PORT': '3306'
     }
 }
 
