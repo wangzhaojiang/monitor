0ba698af8d87a3c9c89dbe6ac8cbf0eaed07e8d1
wzj <wangzhaojiang2013@gmail.com>
2014-10-27 22:33:10 +0800

    update

--- monitor/monitor/state/models.py
+++ monitor/monitor/state/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class cpu(models.Model):
+    #time = models.DateTimeField()
+    time = models.CharField(max_length = 30)
+    user_use = models.FloatField(default = 0.0)
+    system_use = models.FloatField(default = 0.0)
+    all_use = models.FloatField(default = 0.0)
+
+    def __unicode__(self):
+        return self.all_use
+
+
+class diskio(models.Model):
+    time = models.CharField(max_length = 30)
+    pgpgin = models.FloatField(default = 0)
+    pgpgout = models.FloatField(default = 0)
+
+
+class 
