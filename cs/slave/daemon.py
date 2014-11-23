de8ab610afb773ede02c0f118d97e7e2823a640f
wzj <wangzhaojiang2013@gmail.com>
2014-11-24 00:41:41 +0800

    slave file

---
+++ cs/slave/daemon.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#  ------------------------------------
+#  Create date : 2014-11-23 22:54
+#  Author : Wangzhaojiang
+#  Email : wangzhaojiang2013@gmail.com
+#  ------------------------------------
+import socket
+
+def sock():
+    pass
+
+
+
